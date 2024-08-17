e77a4b429bb31b8f8b8fe3cd4543ae1629e1ac18
Renato Daman <dev.makavelii@gmail.com>
2024-08-17 14:02:26 +0200

    Update 1.7

--- Assets/Scripts/ParticlesController.cs
+++ Assets/Scripts/ParticlesController.cs
@@ -1,3 +1,69 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.Linq;
+// using UnityEngine;
+
+// public class ParticlesController : MonoBehaviour
+// {
+//     public Color paintColor;
+
+//     // public float minRadius = 0.05f;
+//     // public float maxRadius = 0.2f;
+//     // public float strength = 1;
+//     // public float hardness = 1;
+//     public float tolerance = 0.01f;
+
+//     [Space]
+//     ParticleSystem part;
+//     List<ParticleCollisionEvent> collisionEvents;
+
+//     void Start()
+//     {
+//         part = GetComponent<ParticleSystem>();
+//         collisionEvents = new List<ParticleCollisionEvent>();
+//     }
+
+//     void OnParticleCollision(GameObject other)
+//     {
+//         int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+//         if (other.TryGetComponent<Paintable>(out var p))
+//         {
+//             for (int i = 0; i < numCollisionEvents; i++)
+//             {
+//                 Vector3 pos = collisionEvents[i].intersection;
+//                 float radius = Random.Range(p.minRadius, p.maxRadius);
+
+//                 // Check if the current paint color is allowed using the modified comparison
+//                 if (p.allowedColors.Any(allowedColor => AreColorsSimilar(allowedColor, paintColor)))
+//                 {
+//                     PaintManager.instance.Paint(p, pos, radius, p.hardness, p.strength, paintColor);
+
+//                     // Check if fully painted
+//                     if (p.IsFullyPainted())
+//                     {
+//                         Debug.Log("The paintable object is fully painted.");
+//                     }
+//                 }
+//                 else
+//                 {
+//                     Debug.Log("Color not allowed");
+//                 }
+//             }
+//         }
+//     }
+
+//     // Method to compare colors with a tolerance
+//     bool AreColorsSimilar(Color color1, Color color2)
+//     {
+//         return Mathf.Abs(color1.r - color2.r) < tolerance &&
+//                Mathf.Abs(color1.g - color2.g) < tolerance &&
+//                Mathf.Abs(color1.b - color2.b) < tolerance &&
+//                Mathf.Abs(color1.a - color2.a) < tolerance;
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,51 +72,71 @@ using UnityEngine;
 public class ParticlesController : MonoBehaviour
 {
     public Color paintColor;
-
-    // public float minRadius = 0.05f;
-    // public float maxRadius = 0.2f;
-    // public float strength = 1;
-    // public float hardness = 1;
     public float tolerance = 0.01f;
+    public int raycastStep = 2; // Only raycast every nth particle
+    //public int paintLayerMask = 1 << 8; // Assuming "PaintableLayer" is on Layer 8
+    public float maxRaycastDistance = 10f; // Maximum distance for raycasts
 
-    [Space]
     ParticleSystem part;
-    List<ParticleCollisionEvent> collisionEvents;
+    ParticleSystem.Particle[] particles;
+    List<Vector3> paintPositions;
+    List<Paintable> paintables;
 
     void Start()
     {
         part = GetComponent<ParticleSystem>();
-        collisionEvents = new List<ParticleCollisionEvent>();
+        particles = new ParticleSystem.Particle[part.main.maxParticles];
+        paintPositions = new List<Vector3>();
+        paintables = new List<Paintable>();
     }
 
-    void OnParticleCollision(GameObject other)
+    void Update()
     {
-        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+        int numParticlesAlive = part.GetParticles(particles);
 
-        if (other.TryGetComponent<Paintable>(out var p))
+        for (int i = 0; i < numParticlesAlive; i += raycastStep)
         {
-            for (int i = 0; i < numCollisionEvents; i++)
+            Vector3 particlePosition = particles[i].position;
+            Vector3 particleVelocity = particles[i].velocity;
+
+            // Perform a raycast in the direction of the particle's velocity
+            if (Physics.Raycast(particlePosition, particleVelocity.normalized, out RaycastHit hit, maxRaycastDistance))
             {
-                Vector3 pos = collisionEvents[i].intersection;
-                float radius = Random.Range(p.minRadius, p.maxRadius);
-
-                // Check if the current paint color is allowed using the modified comparison
-                if (p.allowedColors.Any(allowedColor => AreColorsSimilar(allowedColor, paintColor)))
+                if (hit.collider.TryGetComponent<Paintable>(out var paintable))
                 {
-                    PaintManager.instance.Paint(p, pos, radius, p.hardness, p.strength, paintColor);
-
-                    // Check if fully painted
-                    if (p.IsFullyPainted())
-                    {
-                        Debug.Log("The paintable object is fully painted.");
-                    }
-                }
-                else
-                {
-                    Debug.Log("Color not allowed");
+                    paintPositions.Add(hit.point);
+                    paintables.Add(paintable);
                 }
             }
         }
+
+        // Batch process the paint positions after all raycasts
+        for (int i = 0; i < paintPositions.Count; i++)
+        {
+            Vector3 pos = paintPositions[i];
+            Paintable paintable = paintables[i];
+            float radius = Random.Range(paintable.minRadius, paintable.maxRadius);
+
+            // Check if the current paint color is allowed
+            if (paintable.allowedColors.Any(allowedColor => AreColorsSimilar(allowedColor, paintColor)))
+            {
+                PaintManager.instance.Paint(paintable, pos, radius, paintable.hardness, paintable.strength, paintColor);
+
+                // Check if fully painted
+                if (paintable.IsFullyPainted())
+                {
+                    Debug.Log("The paintable object is fully painted.");
+                }
+            }
+            else
+            {
+                Debug.Log("Color not allowed");
+            }
+        }
+
+        // Clear the lists after processing
+        paintPositions.Clear();
+        paintables.Clear();
     }
 
     // Method to compare colors with a tolerance
